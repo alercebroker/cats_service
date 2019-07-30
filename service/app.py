b66e75f5a7fc65a0675c72777e99ce625ef36114
druzmieres <druzmieres@gmail.com>
2019-07-30 23:02:45 +0000

    add crossmatch all

--- service/app.py
+++ service/app.py
@@ -61,14 +61,23 @@ def crossmatch():
     dec = float(request.args.get('dec'))
     radius = float(request.args.get('radius'))
 
+    return str(crossmatch(catalog, ra, dec, radius))
+
+def crossmatch(catalog, ra, dec, radius):
     match, catalog_columns, columns_units = cone_search(catalog, ra, dec, radius, path)
-    df = pd.DataFrame(match, columns=catalog_columns)
+    try:
+        df = pd.DataFrame(match, columns=catalog_columns)
+    except:
+        return []
     matches = []
     for index, row in df.iterrows():
         obj = dict(zip(catalog_columns, row.values))
         matches.append(obj)
     # this object is a list with one dictionary containing the ra, dec of the closest matching object
-    closest_ra_dec = get_min_distance(matches, catalog, ra, dec)
+    try:
+        closest_ra_dec = get_min_distance(matches, catalog, ra, dec)
+    except:
+        return []
     # get all the fields of the matching object
     ra_cat, dec_cat = map_ra_dec(catalog)
     # this part is stupidly inneficient, i need to learn pandas
@@ -76,11 +85,23 @@ def crossmatch():
     for index, row in df.iterrows():
         if row[ra_cat] == closest_ra_dec[0]['ra'] and row[dec_cat] == closest_ra_dec[0]['dec']:
             result.append(row)
-    return str(result)
+    return result
 
 @app.route('/crossmatch_all')
 def crossmatch_all():
-    return None
+    global catalogs
+    ra = float(request.args.get('ra'))
+    dec = float(request.args.get('dec'))
+    radius = float(request.args.get('radius'))
+    
+    result = []
+    for catalog in catalogs:
+        partial_result = crossmatch(catalog, ra, dec, radius)
+        if partial_result:
+            result_catname = {catalog: partial_result}
+            result.append(result_catname)
+    #should jsonify instead
+    return str(result)
 
 def map_ra_dec(catalog):
     if catalog == 'HSCv2':
@@ -111,9 +132,11 @@ def get_min_distance(matches, catalog, ra, dec):
                 )
         distances.append(dict(distance=point_requested.separation(point_cat).arcsecond, ra=match[ra_cat], dec=match[dec_cat])) #distance, object ra, object dec
     #get minimum distance
-    min_distance = min([float(x['distance']) for x in distances])
-    # list with a dictionary with the min distance, ra and dec
-    return list(filter(lambda x: x['distance'] == min_distance, distances))
+    if distances:
+        min_distance = min([float(x['distance']) for x in distances])
+        # list with a dictionary with the min distance, ra and dec
+        return list(filter(lambda x: x['distance'] == min_distance, distances))
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
